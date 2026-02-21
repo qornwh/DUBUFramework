51688f8b99acf4f2e956538c654b01e6cd53aafe
qornwh <qornwh@gmail.com>
2026-02-21 12:36:59 +0900

    fix : 코드정리, 컨벤션 적용

--- DUBUFrameWork/SessionManager.h
+++ DUBUFrameWork/SessionManager.h
@@ -1,19 +1,29 @@
 #pragma once
 #include "pch.h"
+#include "ConcurrentQueue.h"
+#include "Session.h"
 
-class SessionManager
+namespace DUBU
 {
-	// 여기서만 세션 생성, 등록, 삭제를 관리 / 다른곳에서 금지
-public:
-	SessionManager();
-	SessionManager(const SessionManager& other) = delete;
-	SessionManager(SessionManager&& other) = delete;
-	~SessionManager();
+	class SessionManager
+	{
+		// 여기서만 세션 생성, 등록, 삭제를 관리 / 다른곳에서 금지
+	public:
+		SessionManager();
+		SessionManager(const SessionManager& other) = delete;
+		SessionManager(SessionManager&& other) = delete;
+		~SessionManager();
 
-	SessionManager& operator=(const SessionManager& other) = delete;
-	SessionManager& operator=(SessionManager&& other) = delete;
+		SessionManager& operator=(const SessionManager& other) = delete;
+		SessionManager& operator=(SessionManager&& other) = delete;
 
-private:
-	Uint32 sessionCount_ = 0;
-};
+		void AddSession();
+		void RemoveSession();
+		void GetSession();
+
+	private:
+		DS::ConcurrentQueue<Session> q_;
+		Uint32 sessionCount_ = 0;
+	};
+}
 
