fa3940868f843cc4fc6e46c81dd7fa702952e8d8
qornwh <qornwh@gmail.com>
2026-07-06 01:26:41 +0900

    feat: 캐시용 패킷 추가

--- DUBUFrameWork/BufferManager.cpp
+++ DUBUFrameWork/BufferManager.cpp
@@ -61,3 +61,51 @@ const Set<DUBU::OverlappedPacketBuffer*>& DUBU::PacketManager::GetUseList()
 {
 	return useList_;
 }
+
+DUBU::CachePacketManager::~CachePacketManager()
+{
+    Release();
+}
+
+void DUBU::CachePacketManager::Initialize()
+{
+
+}
+
+void DUBU::CachePacketManager::Release()
+{
+    WriteLockGuard wl(lk_);
+    while (!list_.empty())
+    {
+        CachePacketBuffer* ptr = list_.back();
+        DUBU::Push<CachePacketBuffer>(ptr);
+        list_.pop_back();
+    }
+}
+
+DUBU::CachePacketBuffer* DUBU::CachePacketManager::PopPacketBuffer()
+{
+    WriteLockGuard wl(lk_);
+    if (!list_.empty())
+    {
+        CachePacketBuffer* ptr = list_.back();
+        list_.pop_back();
+        useList_.insert(ptr);
+        return ptr;
+    }
+
+    CachePacketBuffer* ptr = DUBU::Pop<CachePacketBuffer>();
+    ptr->pos_ = ptr->buffer_;
+    ptr->size_ = 0;
+    return ptr;
+}
+
+void DUBU::CachePacketManager::PushPacketBuffer(CachePacketBuffer* ptr)
+{
+    WriteLockGuard wl(lk_);
+    list_.push_back(ptr);
+    useList_.erase(ptr);
+
+    ZeroMemory(ptr->pos_, ptr->size_);
+    ptr->size_ = 0;
+}
