f30b784bfa688572d4a938e650e350c8d75deff8
qornwh <qornwh@gmail.com>
2026-07-09 13:51:34 +0900

    fix: 파일 주석 정리

--- DUBUFrameWork/CachePacket.h
+++ DUBUFrameWork/CachePacket.h
@@ -13,16 +13,4 @@ namespace DUBU
         Uint32 sequenceNo = 0;
         bool isKeep = false;
     };
-
-  //  struct RepeatPacketState
-  //  {
-  //      Uint32 currentRepeatSeq = 0;
-  //      Uint32 lastRepeatSeq = 0;
-  //      Uint64 cacheRepeatCount = 0;
-  //      Uint32 sendRepeatSeq = 0;
-
-		//Uint32 UpdateSendSequenceNo();
-  //      Uint32 GetRecvSequenceNo() const;
-  //      Uint32 GetSendSequenceNo() const;
-  //  };
 };
