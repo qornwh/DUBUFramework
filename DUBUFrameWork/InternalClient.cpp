8dd06d75bd003226e79b399e8de1fcc65933e45b
qornwh <qornwh@gmail.com>
2026-08-18 20:03:25 +0900

    fix : InternalClient 제어 겹치는 부분 lock 처리

--- DUBUFrameWork/InternalClient.cpp
+++ DUBUFrameWork/InternalClient.cpp
@@ -114,6 +114,8 @@ bool DUBU::InternalClient::Dispatch()
 
 void DUBU::InternalClient::OnRecvFrom(const SOCKADDR_IN& addr, Uint8* ptr, Uint16 size)
 {
+    WriteLockGuard wl(lock_);
+
     OverlappedPacketBuffer* opb = reinterpret_cast<OverlappedPacketBuffer*>(ptr);
 
     auto buffer = opb->buffer_;
@@ -543,17 +545,20 @@ void DUBU::InternalClient::SendPacket(Uint8* buffer, Uint8 code, Uint16 size, co
 {
     if (rudpSocket_ == nullptr) return;
 
-    // 패킷 메모리 할당
     Uint32 offset = sizeof(Packet::PacketHeader);
-    OverlappedPacketBuffer* opb = PacketManager::GetInstance().PopPacketBuffer();
-    Packet::PacketHeader* header = reinterpret_cast<Packet::PacketHeader*>(opb->buffer_);
 
+    // 크기 초과면 청크로 분할. 
     if (size + subHeaderSize + offset > PACKET_MAX_SIZE)
     {
-        // 이때는 청크로 나눠서 보내본다.
         SendPacketChunk(buffer, code, size, subHeader, subHeaderSize);
         return;
     }
+
+    WriteLockGuard wl(lock_);
+
+    // 패킷 메모리 할당
+    OverlappedPacketBuffer* opb = PacketManager::GetInstance().PopPacketBuffer();
+    Packet::PacketHeader* header = reinterpret_cast<Packet::PacketHeader*>(opb->buffer_);
 
     // 패킷 헤더 옵션 설정
     header->flags_ = Packet::PacketHeaderFlag::NONE;
@@ -694,6 +699,8 @@ void DUBU::InternalClient::RepeatPongMessage(Uint8* ptr, Uint16 size)
 
 void DUBU::InternalClient::CheckPending()
 {
+    WriteLockGuard wl(lock_);
+
     // 시간체크
     Uint32 now = GetRelativeTimeMs();
 
