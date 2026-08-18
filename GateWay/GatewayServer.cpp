e96ad15699dbe2492916a0c21f0871b5b79f923e
qornwh <qornwh@gmail.com>
2026-08-19 01:25:11 +0900

    fix : 에코 게이트웨이 오브젝트풀 반환 누락

--- GateWay/GatewayServer.cpp
+++ GateWay/GatewayServer.cpp
@@ -24,7 +24,7 @@ GatewayServer::~GatewayServer()
             client->Disconnect();
         }
 
-        delete client;
+        DUBU::Push<DUBU::InternalClient>(client);
     }
 
     internalClientList_.clear();
